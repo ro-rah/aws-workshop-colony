a809ff23fee109ed3a8d1a56a0b8365575b55a28
Ronak Rahman <ronak.r@qualisystems.com>
2021-08-25 12:34:49 -0500

    simple minecraft server app with eula

---
+++ applications/mcserver/minecraft_server-command.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'Start Minecraft server'
+java -jar minecraft_server.jar --nogui
---
+++ applications/mcserver/minecraft_server.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo '=============== Staring init script for Promotions Manager UI ==============='
+
+# save all env for debugging
+printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
+
+echo '==> Download Minecraft server version 1.16.2'
+apt-get update
+apt install curl -y
+curl -sL https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar | bash -
+echo -e '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+\n#Wed Aug 25 02:30:24 UTC 2021 \neula=true' >> eula.txt
