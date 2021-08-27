e3874f23bce76ab603e650577a04d959b55df4e6
Ronak Rahman <ronak.r@qualisystems.com>
2021-08-27 11:19:42 -0500

    curl of server jar was wrong

--- applications/mcserver/minecraft_server.sh
+++ applications/mcserver/minecraft_server.sh
@@ -7,6 +7,7 @@ printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 echo '==> Download Minecraft server version 1.16.2'
 apt-get update
 apt install curl -y
-curl -sL https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar | bash -
+curl https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar --output server.jar
+
 echo -e '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
-\n#Wed Aug 25 02:30:24 UTC 2021 \neula=true' >> eula.txt
+\n#Wed Aug 25 02:30:24 UTC 2021 \neula=true' > eula.txt
