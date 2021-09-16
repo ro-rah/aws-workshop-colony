4b18b8270c09612d10db363efdbeeed87058ece8
Ronak Rahman <ronak.r@qualisystems.com>
2021-09-16 15:01:33 -0500

    missing slash on sed script

--- applications/mcserver/minecraft_server.sh
+++ applications/mcserver/minecraft_server.sh
@@ -12,26 +12,24 @@ curl https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b28978
 echo -e '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
 \n#Wed Aug 25 02:30:24 UTC 2021 \neula=true' > eula.txt
 
-echo '==> Updating gamemode, current value is $GAMEMODE'
+echo "==> Updating gamemode, current value is $GAMEMODE"
 if [ "$GAMEMODE" == "creative" ]; then
-    sed 's/gamemode=survival/gamemode=creative' /server.properties
+    sed 's/gamemode=survival/gamemode=creative/' /server.properties
 elif [ "$GAMEMODE" == "survival" ]; then
     echo "Nothing to do here."
 elif [ "$GAMEMODE" == "adventure" ]; then
-    sed 's/gamemode=survival/gamemode=adventure' /server.properties
+    sed 's/gamemode=survival/gamemode=adventure/' /server.properties
 elif [ "$GAMEMODE" == "spectator" ]; then
-    sed 's/gamemode=survival/gamemode=spectator' /server.properties
+    sed 's/gamemode=survival/gamemode=spectator/' /server.properties
 else
 	echo "You must state one of the following gamemodes: creative, survival, adventure, or spectator"
 fi
 
-echo '==> Updating allow-flight current value is $ALLOWFLIGHT'
+echo "==> Updating allow-flight current value is $ALLOWFLIGHT"
 if [ "$ALLOWFLIGHT" == "true" ]; then
-    #sed 's/allow-flight=false/allow-flight=true' /server.properties
-	echo "true path executed"
+    sed 's/allow-flight=false/allow-flight=true/' /server.properties
 elif [ "$ALLOWFLIGHT" == "false" ]; then
-    #sed 's/allow-flight=false/allow-flight=false' /server.properties
-	echo "false path executed"
+    sed 's/allow-flight=false/allow-flight=false/' /server.properties
 else
 	echo "Value for ALLOWFLIGHT must be true or false"
 fi
