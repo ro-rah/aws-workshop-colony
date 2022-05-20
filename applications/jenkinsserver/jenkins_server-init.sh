1ca7e40aa0d629357240c978d30d8e8741989d4f
ronak <ronak@example.com>
2022-05-20 18:13:31 -0500

    correct archive path

--- applications/jenkinsserver/jenkins_server-init.sh
+++ applications/jenkinsserver/jenkins_server-init.sh
@@ -52,9 +52,7 @@ echo 'export M2=$M2_HOME/bin' |  sudo tee -a /etc/profile.d/maven.sh
 echo 'export PATH=$M2:$PATH' |  sudo tee -a /etc/profile.d/maven.sh
 
 echo '==> Extract artifact to /tmp/'
-sudo tar xvzf $ARTIFACTS_PATH/jenkins/jenkins.tar.gz --directory  /tmp
-sudo $ARTIFACTS_PATH/jenkins/jenkins.tar.gz /tmp
-
+sudo tar xvzf $ARTIFACTS_PATH/jenkins.tar.gz --directory  /var
 
 #enable amazon epel and install demonize
 sudo yum-config-manager --enable epel
