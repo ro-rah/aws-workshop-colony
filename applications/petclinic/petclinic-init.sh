ce82294d94f5b52d4d93faba958a08ca0666eb23
ronak <ronak@example.com>
2022-05-22 15:46:18 -0500

    add back setup functionality

--- applications/petclinic/petclinic-init.sh
+++ applications/petclinic/petclinic-init.sh
@@ -5,36 +5,29 @@ echo '=============== Staring init script for PetClinic Server ==============='
 printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
 #install java
-##sudo yum -y install java-11-openjdk-devel
+sudo yum -y install java-11-openjdk-devel
 
 
 #install git
-#sudo yum -y install git
+sudo yum -y install git
 
 #Install JFrog CLI
-##sudo echo "[jfrog-cli]" > jfrog-cli.repo;
-##sudo echo "name=jfrog-cli" >> jfrog-cli.repo;
-##sudo echo "baseurl=https://releases.jfrog.io/artifactory/jfrog-rpms" >> jfrog-cli.repo;
-##sudo echo "enabled=1" >> jfrog-cli.repo;
-##sudo rpm --import https://releases.jfrog.io/artifactory/jfrog-gpg-public/jfrog_public_gpg.key
-##sudo mv jfrog-cli.repo /etc/yum.repos.d/;
-##sudo yum install -y jfrog-cli-v2-jf;
+sudo echo "[jfrog-cli]" > jfrog-cli.repo;
+sudo echo "name=jfrog-cli" >> jfrog-cli.repo;
+sudo echo "baseurl=https://releases.jfrog.io/artifactory/jfrog-rpms" >> jfrog-cli.repo;
+sudo echo "enabled=1" >> jfrog-cli.repo;
+sudo rpm --import https://releases.jfrog.io/artifactory/jfrog-gpg-public/jfrog_public_gpg.key
+sudo mv jfrog-cli.repo /etc/yum.repos.d/;
+sudo yum install -y jfrog-cli-v2-jf;
 
 #import creds
-echo "value for jfrog token: ${torque.parameters.jfrogtoken}"
-echo "value for jfrog token: ${jfrogtoken}"
-echo "value for jfrog token: $torque.parameters.jfrogtoken"
-echo "value for jfrog token: $jfrogtoken"
-echo ${torque.parameters.jfrogtoken}
-echo ${jfrogtoken}
-echo $torque.parameters.jfrogtoken
-echo $jfrogtoken
-##jf config import $JFROGTOKEN
+echo $JFROGTOKEN
+jf config import $JFROGTOKEN
 
 
 #download artifact
-##echo $PETCLINICVERSION
-##jf rt dl --flat default-generic-local/com/petclinic/spring-petclinic-$PETCLINICVERSION.jar
+echo $PETCLINICVERSION
+jf rt dl --flat default-generic-local/com/petclinic/spring-petclinic-$PETCLINICVERSION.jar
 
 #start PetClinic
-##sudo nohup java -jar spring-petclinic-2.6.0-SNAPSHOT.jar &
+sudo nohup java -jar spring-petclinic-2.6.0-SNAPSHOT.jar &
